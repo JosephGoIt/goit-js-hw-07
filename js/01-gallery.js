88bcffc2e7d91aacd5d70d17dea6243c726b6ea4
Joseph Camacho <spongkj@gmail.com>
2024-02-14 11:38:12 +0800

    completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,35 @@
-import * as basicLightbox from 'basiclightbox';
-
+// import * as basicLightbox from 'basiclightbox';
 import { galleryItems } from './gallery-items.js';
 
+console.log(galleryItems);
 
 // Create gallery markup
 const gallery = document.querySelector('.gallery');
 
 galleryItems.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('gallery__item');
-  
-    listItem.innerHTML = `
-      <a class="gallery__link" href="${item.original}">
-        <img
-          class="gallery__image"
-          src="${item.preview}"
-          data-source="${item.original}"
-          alt="${item.description}"
-        />
-      </a>
-    `;
-  
-    gallery.appendChild(listItem);
+  let li = `
+  <li class="gallery__item">
+    <a class="gallery__link" href="">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>
+  `;  
+  gallery.innerHTML += li;
   });
 
 // Event delegation to open modal
- gallery.addEventListener('click', event => {
-   event.preventDefault(); // Prevent default behavior of anchor tag
+  gallery.addEventListener('click', event => {
+    event.preventDefault(); // Prevent default behavior of anchor tag
 
-   if (event.target.classList.contains('gallery__image')) {
-     const imageUrl = event.target.dataset.source;
+    if (event.target.classList.contains('gallery__image')) {
+      const imageUrl = event.target.dataset.source;
       const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Image">`);
-      instance.show();
-   }
- });
+       instance.show();
+    }
+  });
 
-console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('gallery__item');
+
+  listItem.innerHTML = `
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  `;
+
+  gallery.appendChild(listItem);
+});
+
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
